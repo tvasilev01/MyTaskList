2514cacdc9ce184c304273cc55c85b5e4e16e132
Tony <tonovasilev@gmail.com>
2024-05-17 21:26:34 +0300

    Create Task.java class fot task management. Includes fields for title, description, deadline, and priority. Constructor to initialize all fields. Getter and setter methods for each fields. toString() method to represent the task as a string in the format: "title - description - deadline - Priority: priority".

---
+++ src/Task.java
@@ -0,0 +1,54 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Task {
+
+    private String title;
+    private String description;
+    private Date deadline;
+    private int priority;
+
+    public Task(String title, String description, Date deadline, int priority){
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String toString(){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return String.format("%s - %s - %s - Priority: %d", title, description, sdf.format(deadline), priority);
+    }
+}

d655981cca07e14be51ba82fc1212306fd5d9c19
Tony <tonovasilev@gmail.com>
2024-05-17 16:16:35 +0300

    Create first commit.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My Task Manager");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+
+
+
+        frame.setVisible(true);
+    }
+}

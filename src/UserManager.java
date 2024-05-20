42490179a9a1291895699a2671b5c0dab536a2db
Niray <nirayveli@gmail.com>
2024-05-20 16:07:24 +0300

    User panel manager

---
+++ src/UserManager.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserManager {
+
+    private Map<String, User> users = new HashMap<>();
+
+    public boolean addUser(String username, String password){
+        if (users.containsKey(username)){
+            return false;
+        }
+        users.put(username, new User(username, password));
+        return true;
+    }
+
+    public User authenticate(String name, String password){
+        User user = users.get(username);
+        if(user != null && user.getPassword().equals(password)){
+            return user;
+        }
+        return null;
+    }
+}
